3c88be54b300226481ae288ce820afe0e96993ac
mtsgi <oq@live.jp>
2019-03-22 03:23:10 +0900

    Add a "Sign Up" [0.1.0]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new()
+    end
+
+    def create
+        @user = User.new(params[:user].permit(:name, :authorid, :password, :profile))
+        if( @user.save )
+            session[:user_id] = @user.id
+            redirect_to( :root, notice: "ユーザー登録が完了しました。" )
+        else
+            render( "/users/new" )
+        end
+    end
+
+    def index
+        @users = User.all()
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates( :name, presence: { message: "名前を入力してください。" } )
+    validates( :authorid, presence: { message: "作者IDを入力してください。" }, uniqueness: { message: "その作者IDはすでに登録されています。" } )
+    validates( :password, presence: { on: :create, message: "パスワードを入力してください。" }, confirmation: { allow_blank: true, message: "パスワード確認が間違っています。" } )
 end
