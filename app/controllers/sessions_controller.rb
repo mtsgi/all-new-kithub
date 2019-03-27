38917cdecd1a792908c683bf6e7526b8fa00f81b
mtsgi <oq@live.jp>
2019-03-27 19:32:30 +0900

    Add Login Function [0.1.0]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,17 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by_authorid(params[:authorid])
         if( @user && @user.authenticate(params[:password]) )
-            session[:authorid] = @user.authorid
+            session[:user_id] = @user.id
+            flash.notice = @user.authorid + "としてログインしました"
+            redirect_to("/my")
         else
             flash.notice = "ログインに失敗しました"
+            redirect_to("/users/login")
         end
     end
 
     def destroy
-        session.delete(:authorid)
+        session.delete(:user_id)
         flash.notice = "ログアウトしました"
         redirect_to(:root)
     end
